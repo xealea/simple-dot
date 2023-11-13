e7d6213f605d8a1d7586bd9cdab26c46fa8a9f43
ʟᴇᴀ <xealea@proton.me>
2023-11-14 00:02:41 +0700

    Update herbscreenshot.sh

--- .config/herbstluftwm/bin/herbscreenshot.sh
+++ .config/herbstluftwm/bin/herbscreenshot.sh
@@ -4,57 +4,63 @@
 # Email: xealea@xedev.my.id
 # Copyright © 2023 Lea <xealea@proton.me>
 
-# Constants
-CONFIG_DIR="$HOME/.config/herbstluftwm"
-SCREENSHOT_DIR="$(xdg-user-dir PICTURES)/Screenshots"
+# Configuration
+DIR="$HOME/.config/herbstluftwm"
 NOTIFY_ICON="/usr/share/icons/dunst/picture.png"
 TIMER_ICON="/usr/share/icons/dunst/timer.png"
-SOUND_FILE="/usr/share/sounds/freedesktop/stereo/screen-capture.oga"
-
-# Variables
-TIME=$(date +%Y-%m-%d-%H-%M-%S)
-GEOMETRY=$(xrandr | grep 'current' | head -n1 | cut -d',' -f2 | tr -d '[:blank:],current')
-FILE="Screenshot_${TIME}_${GEOMETRY}.png"
-TEMP_FILE="${SCREENSHOT_DIR}/temp_${TIME}.png"
-NOTIFY_CMD_SHOT="dunstify -u low --replace=699 -i $NOTIFY_ICON"
-ROFI_COMMAND="rofi -theme $CONFIG_DIR/rofi/themes/screenshot.rasi"
+ROFI_THEME="$DIR/rofi/themes/screenshot.rasi"
 
 # Functions
+notify() {
+  dunstify -u low --replace=699 -i "$NOTIFY_ICON" "$1"
+}
+
 countdown() {
-    for sec in $(seq "$1" -1 1); do
-        dunstify -t 1000 --replace=699 -i $TIMER_ICON "Taking shot in: $sec"
-        sleep 1
-    done
+  for ((sec = $1; sec > 0; sec--)); do
+    notify "Taking shot in: $sec"
+    sleep 1
+  done
 }
 
 convert_image() {
-    convert "$1" \( +clone -background black -shadow 25x30+0+0 \) +swap -background '#b6beca' -layers merge "$2"
+  convert "$1" \( +clone -background black -shadow 25x30+0+0 \) +swap -background '#b6beca' -layers merge "$2"
 }
 
-notify_view() {
-    $NOTIFY_CMD_SHOT "Copied to clipboard."
-    paplay $SOUND_FILE &>/dev/null &
-    viewnior "$SCREENSHOT_DIR/$FILE"
-    if [[ -e "$SCREENSHOT_DIR/$FILE" ]]; then
-        $NOTIFY_CMD_SHOT "Screenshot Saved."
-    else
-        $NOTIFY_CMD_SHOT "Screenshot Deleted."
-    fi
+take_screenshot() {
+  maim -u -f png "$1"
 }
 
-# Create screenshot directory if not exists
-if [[ ! -d "$SCREENSHOT_DIR" ]]; then
-    mkdir -p "$SCREENSHOT_DIR" || { echo "Error creating directory."; exit 1; }
+# Main
+time=$(date +%Y-%m-%d-%H-%M-%S)
+geometry=$(xrandr --current | awk '/current/ {print $8}')
+dir="$(xdg-user-dir PICTURES)/Screenshots"
+file="Screenshot_${time}_${geometry}.png"
+temp_file="${dir}/temp_${time}.png"
+
+if [[ ! -d "$dir" ]]; then
+  mkdir -p "$dir"
 fi
 
-# Main execution
 case "$1" in
-    "--now") shotnow ;;
-    "--in5") shot_with_countdown 5 ;;
-    "--in10") shot_with_countdown 10 ;;
-    "--win") shotwin ;;
-    "--area") shotarea ;;
-    *) echo -e "Available Options : --now --in5 --in10 --win --area" ;;
+  --now)
+    take_screenshot "$dir/$temp_file" && convert_image "$dir/$temp_file" "$dir/$file" && notify "Copied to clipboard." && notify_view "$dir/$file"
+    ;;
+  --in5)
+    countdown 5 && take_screenshot "$dir/$temp_file" && convert_image "$dir/$temp_file" "$dir/$file" && notify_view "$dir/$file"
+    ;;
+  --in10)
+    countdown 10 && take_screenshot "$dir/$temp_file" && convert_image "$dir/$temp_file" "$dir/$file" && notify_view "$dir/$file"
+    ;;
+  --win)
+    take_screenshot "$dir/$temp_file" -i "$(xdotool getactivewindow)" && convert_image "$dir/$temp_file" "$dir/$file" && notify_view "$dir/$file"
+    ;;
+  --area)
+    take_screenshot "$dir/$temp_file" -s -b 2 -c 0.35,0.55,0.85,0.25 && convert_image "$dir/$temp_file" "$dir/$file" && notify_view "$dir/$file"
+    ;;
+  *)
+    echo "Available Options: --now --in5 --in10 --win --area"
+    exit 1
+    ;;
 esac
 
 exit 0
